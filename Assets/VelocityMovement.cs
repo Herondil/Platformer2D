447b8e299cc7bfdc1e4410613a9f301dc8e69ea4
Maxens Dubois <maxens.dubois@gmail.com>
2022-08-25 16:49:57 +0200

    platformes mobile

--- Assets/VelocityMovement.cs
+++ Assets/VelocityMovement.cs
@@ -6,16 +6,18 @@ public class VelocityMovement : MonoBehaviour
 {
 
     public Rigidbody2D rb2d;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float GravityScaleOnFall;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rb2d.velocity.y < 0)
+        {
+            rb2d.gravityScale = GravityScaleOnFall;
+        }
+        else
+        {
+            rb2d.gravityScale = 1;
+        }
     }
 }
