cbd7d6a38862bb1a69ddaeb6f5307b734688a0d1
Maxens Dubois <maxens.dubois@gmail.com>
2022-08-26 12:37:22 +0200

    Animator Parameter Scripting

--- Assets/VelocityMovement.cs
+++ Assets/VelocityMovement.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class VelocityMovement : MonoBehaviour
 {
 
+    //par ici po
+
     public Rigidbody2D rb2d;
     public float GravityScaleOnFall;
+    public Animator _animator;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (rb2d.velocity.y < 0)
+        //bon par rapport au Game Design
+        if (rb2d.velocity.y < -1)
         {
             rb2d.gravityScale = GravityScaleOnFall;
+            _animator.SetBool("Falling?", true);
         }
         else
         {
+            //je touche le sol ?
             rb2d.gravityScale = 1;
+            _animator.SetBool("Falling?", false);
         }
     }
+
+
 }
